40b91c586442da63be17b7c5de6fdc6fc0b2012b
victor chiazam <vchiazam@gmail.com>
2024-10-21 01:08:19 +0100

    feat: Implement password validation with security rules

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -1,51 +1,50 @@
-import { VariantProps, cva } from 'class-variance-authority'
-import { DetailedHTMLProps, HTMLAttributes } from 'react'
+import { VariantProps, cva } from "class-variance-authority"
+import { DetailedHTMLProps, HTMLAttributes } from "react"
 
-import { cn } from '@/utils'
-import { jaka } from '@/fonts'
+import { cn } from "@/utils"
 
-const headerClasses = cva(['font-bold', jaka.className], {
-  variants: {
-    size: {
-      h1: ['text-[42px] leading-[52px]'],
-      h2: ['text-[30px] leading-[45px]'],
-      h3: ['text-[20px] sm:text-[24px] leading-[36px]'],
-      h4: ['text-[20px] leading-[30px]'],
-      h5: ['text-[18px] leading-[27px]'],
-      h6: ['text-[14px] leading-[24px]'],
-    },
+const headerClasses = cva(["font-bold", "font-jaka"], {
+ variants: {
+  size: {
+   h1: ["text-[42px] leading-[52px]"],
+   h2: ["text-[30px] leading-[45px]"],
+   h3: ["text-[20px] sm:text-[24px] leading-[36px]"],
+   h4: ["text-[20px] leading-[30px]"],
+   h5: ["text-[18px] leading-[27px]"],
+   h6: ["text-[14px] leading-[24px]"],
   },
-  defaultVariants: {
-    size: 'h2',
-  },
+ },
+ defaultVariants: {
+  size: "h2",
+ },
 })
 
 export interface HeadingProps
-  extends DetailedHTMLProps<
-      HTMLAttributes<HTMLHeadingElement>,
-      HTMLHeadingElement
-    >,
-    VariantProps<typeof headerClasses> {
-  as?: HeaderTypes
+ extends DetailedHTMLProps<
+   HTMLAttributes<HTMLHeadingElement>,
+   HTMLHeadingElement
+  >,
+  VariantProps<typeof headerClasses> {
+ as?: HeaderTypes
 }
-type HeaderTypes = 'h1' | 'h2' | 'h3' | 'h4'
+type HeaderTypes = "h1" | "h2" | "h3" | "h4"
 
 function Heading({
-  children,
-  size,
-  as = 'h2',
-  style,
-  className = 'font-bold',
+ children,
+ size,
+ as = "h2",
+ style,
+ className = "font-bold",
 }: HeadingProps) {
-  const allowedTypes = ['h1', 'h2', 'h3', 'h4', 'h5']
-  const Comp = allowedTypes.includes(as) ? as : ('h2' as const)
+ const allowedTypes = ["h1", "h2", "h3", "h4", "h5"]
+ const Comp = allowedTypes.includes(as) ? as : ("h2" as const)
 
-  const classNames = cn(headerClasses({ size }), className)
-  return (
-    <Comp className={classNames} style={style}>
-      {children}
-    </Comp>
-  )
+ const classNames = cn(headerClasses({ size }), className)
+ return (
+  <Comp className={classNames} style={style}>
+   {children}
+  </Comp>
+ )
 }
 
 export default Heading
